59e6c9e15bd25c5c7895864446aa13f48a11c141
Joboson Solomon Chiakonam <solobachi02@gmail.com>
2024-02-26 10:08:00 +0100

    setup files

---
+++ app/app.py
@@ -0,0 +1,25 @@
+import pymongo
+from flask import Flask
+from flask import request,jsonify
+from flask_cors import CORS
+from rembg import remove
+from PIL import Image
+
+
+# create an app
+app = Flask(__name__) 
+
+
+# routes 
+# default route
+# @app.route("/")
+# def incde
+
+
+
+
+
+
+# run the app
+if __name__ == "__main__":
+    app.run()
